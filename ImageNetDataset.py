6cd53dd747175b02a5fd0144ba0f429667e14785
ShowLo <945519267@qq.com>
2019-05-09 12:00:32 +0800

    change data transformation

--- ImageNetDataset.py
+++ ImageNetDataset.py
@@ -16,8 +16,7 @@ def ImageNetDataset(args):
     # data transform
     data_transforms = {
         'train': transforms.Compose([
-            transforms.Resize(256),
-            transforms.RandomCrop(224),
+            transforms.RandomResizedCrop(224),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
